4a67fe998d354a5df22eb977912bd5dfbb9aea88
Jim B. Matheson <jimbmatheson@gmail.com>
2020-09-14 21:15:58 -0400

    added second screen and tied it to toggle button

--- weather_time_touch_1/weather_time_touch_1/weather_time_touch_1.ino
+++ weather_time_touch_1/weather_time_touch_1/weather_time_touch_1.ino
@@ -9,7 +9,6 @@
 
 #include <Wire.h>
 #include "RTClib.h"
-RTC_DS3231 rtc;
 
 #define YP A2  // must be an analog pin, use "An" notation!
 #define XM A3  // must be an analog pin, use "An" notation!
@@ -33,22 +32,23 @@ RTC_DS3231 rtc;
 #define YELLOW  0xFFE0
 #define WHITE   0xFFFF
 
-Elegoo_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
-
 #define DHTPIN 52                    // what pin we're connected to DHT Sensor
 #define DHTTYPE DHT11                // DHT 11
-DHT dht(DHTPIN, DHTTYPE);   
+
+RTC_DS3231 rtc;
+
+DHT dht(DHTPIN, DHTTYPE);  
+
+Elegoo_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
 
 Elegoo_GFX_Button button1;
 Elegoo_GFX_Button button2;
 Elegoo_GFX_Button button3;
 
-// For better pressure precision, we need to know the resistance
-// between X+ and X- Use any multimeter to read it
-// For the one we're using, its 300 ohms across the X plate
 TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
 
 char daysOfTheWeek[7][12] = {"Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+uint8_t currScreen = 0; // 0 (Initial Screen) | 1 (Options Screen)
 
 void setup(void) {
   Wire.begin();
@@ -65,45 +65,37 @@ void setup(void) {
 
   uint16_t identifier = 0x9341;
   tft.begin(identifier);
+  
   tft.setRotation(3);
   tft.fillScreen(BLACK);
-  tft.setCursor(15,30);
-  tft.println("TEMPERATURE");
-  
-  tft.setCursor(90,30);
-  tft.println("HUMIDITY");
 
-  tft.setCursor(155,30);
-  tft.println("HEAT INDEX");
-
-  button1.initButton(&tft, 60, 200, 90, 30, BLUE, WHITE, BLACK, "Weather", 1);
-  button1.drawButton();
-
-  button2.initButton(&tft, 155, 200, 90, 30, BLUE, WHITE, BLACK, "Time", 1);
-  button2.drawButton();
-
-  button3.initButton(&tft, 250, 200, 90, 30, BLUE, WHITE, BLACK, "Options", 1);
-  button3.drawButton();
+  showInitialScreen();
 }
 
 void loop(void) {
 
-  float t = getTemperature(true);
-  float h = getHumidity();
-  float hI = getHeatIndex();
-
-
-  tft.setCursor(15,45);
-  tft.setTextColor(WHITE, BLACK);
-  tft.print(t);
+  if(currScreen == 0) {  
+    float t = getTemperature(true);
+    float h = getHumidity();
+    float hI = getHeatIndex();
   
-  tft.setCursor(90,45);
-  tft.setTextColor(WHITE, BLACK);
-  tft.print(h);
+    tft.setCursor(15,45);
+    tft.setTextColor(YELLOW, BLACK);
+    tft.setTextSize(2);
+    tft.print(t);
+    
+    tft.setCursor(90,45);
+    tft.print(h);
+  
+    tft.setCursor(155,45);
+    tft.print(hI); 
 
-  tft.setCursor(155,45);
-  tft.setTextColor(WHITE, BLACK);
-  tft.print(hI);
+    tft.setCursor(15,90);
+    tft.setTextSize(3);
+    
+    printTime();
+  }
+
     
   // a point object holds x y and z coordinates
   TSPoint p = ts.getPoint();
@@ -111,13 +103,37 @@ void loop(void) {
   pinMode(YP, OUTPUT);
 
   if (p.z > ts.pressureThreshhold) {
-     Serial.print("X = "); Serial.print(p.x);
-     Serial.print("\tY = "); Serial.print(p.y);
      Serial.print("\tPressure = "); Serial.println(p.z);
+
+     if(currScreen == 0) {
+       if(p.x >= 143 && p.x <= 364 && p.y >= 110 && p.y <= 189) {
+         currScreen = 1;
+         showOptionsScreen();
+       } else {
+         Serial.print("X = "); Serial.print(p.x);
+         Serial.print("\tY = "); Serial.print(p.y);
+       }      
+     } else if (currScreen == 1){
+        currScreen = 0;
+        showInitialScreen();
+     }
   }
-  
-  tft.setCursor(15,90);
-  printTime();
+}
+
+void showInitialScreen() {
+  button3.initButton(&tft, 270, 220, 90, 30, BLUE, WHITE, BLACK, "Options", 1);
+  button3.drawButton();
+}
+
+void showOptionsScreen() {
+  tft.fillScreen(BLACK);
+  tft.setCursor(25, 100);
+  tft.setTextColor(WHITE);
+  tft.setTextSize(2);
+  tft.println("Options");
+
+  button3.initButton(&tft, 270, 220, 90, 30, BLUE, WHITE, BLACK, "Save", 1);
+  button3.drawButton();
 }
 
 float getTemperature(bool tF) {
@@ -137,22 +153,22 @@ float getHeatIndex() {
 void printTime() {
     DateTime now = rtc.now();
 
-    tft.setCursor(15,145);
-    tft.println("Current Date & Time: ");
-
-    tft.setCursor(15, 160);
-    tft.print(now.year(), DEC);
-    tft.print('/');
+    tft.setCursor(15, 145);
+    tft.setTextColor(WHITE,BLACK);
+    tft.println(daysOfTheWeek[now.dayOfTheWeek()]);
     tft.print(now.month(), DEC);
     tft.print('/');
     tft.print(now.day(), DEC);
-    tft.print(" (");
-    tft.print(daysOfTheWeek[now.dayOfTheWeek()]);
-    tft.print(") ");
+    tft.print('/');
+    tft.print(now.year(), DEC);
+    tft.println();
     tft.print(now.hour(), DEC);
     tft.print(':');
-    tft.print(now.minute(), DEC);
+    uint8_t m = now.minute();
+    tft.print(m);
     tft.print(':');
-    tft.print(now.second(), DEC);
+    uint8_t s = now.second();
+    tft.print(s);
     tft.println();
+    delay(100);
 }
